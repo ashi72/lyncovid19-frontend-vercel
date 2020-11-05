5c9efc81603336c7d9e5a3441ebbf413d745df1d
ashi72 <32313598+ashi72@users.noreply.github.com>
2020-11-05 15:52:31 -0800

    updated auth0 tuturial coade

--- src/auth0-provider-with-history.js
+++ src/auth0-provider-with-history.js
@@ -6,23 +6,23 @@ import {Auth0Provider} from '@auth0/auth0-react';
 const Auth0ProviderWithHistory = ({children}) => {
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const audience = process.env.REACT_APP_AUDIENCE;
+// const audience = process.env.REACT_APP_AUDIENCE;
 console.log(domain);
 console.log(clientId);
 
 
 const history = useHistory();
 
-const onRedirectCallback = (appState) => {
-  history.push(appState?.returnTo || window.location.pathname);
-};
+// const onRedirectCallback = (appState) => {
+//   history.push(appState?.returnTo || window.location.pathname);
+// };
+// REACT_APP_AUDIENCE=https://local-covid-backend-api
+// onRedirectCallback = {onRedirectCallback}
 
 return(
   <Auth0Provider domain = {domain}
     clientId={clientId}
     redirectUri = {window.location.origin}
-    onRedirectCallback = {onRedirectCallback}
-    audience = {audience}
     >
     {children}
   </Auth0Provider>
